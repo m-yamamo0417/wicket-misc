a8dabe1c3dc10283f66e59929d8f3859e81d5034
m_yamamo0417 <m.yamamo0417@gmail.com>
2012-03-13 17:09:30 +0000

    first commit

---
+++ src/main/java/com/myamamoto/AppendErrorClassOnErrorBehavior.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2012 m_yamamo0417
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.myamamoto;
+
+import org.apache.wicket.Component;
+import org.apache.wicket.behavior.Behavior;
+import org.apache.wicket.markup.ComponentTag;
+
+public class AppendErrorClassOnErrorBehavior
+    extends Behavior {
+
+    public static final String ATTRIBUTE_CLASS = "class";
+
+    private final String errorClass;
+    private final String errorClassWithComma;
+
+    public AppendErrorClassOnErrorBehavior() {
+	this("error");
+    }
+
+    public AppendErrorClassOnErrorBehavior(String errorClass) {
+	this.errorClass = errorClass;
+	this.errorClassWithComma = ", " + errorClass;
+    }
+
+    @Override
+    public void onComponentTag(Component arg0, ComponentTag arg1) {
+	super.onComponentTag(arg0, arg1);
+	String orgClass = arg1.getAttribute(ATTRIBUTE_CLASS);
+
+	if(arg0.hasErrorMessage()){
+	    String newClass;
+	    boolean existsOrgClass = null != orgClass && !orgClass.isEmpty();
+	    if(existsOrgClass){
+		newClass = orgClass + this.errorClassWithComma;
+	    }else{
+		newClass = this.errorClass;
+	    }
+	    arg1.put(ATTRIBUTE_CLASS, newClass);
+	}else{
+	    boolean existsOrgClass =
+		null != orgClass
+		&& !orgClass.isEmpty()
+		&& !this.errorClass.equals(orgClass);
+	    if(existsOrgClass){
+		arg1.put(ATTRIBUTE_CLASS,
+			 orgClass.replaceAll(this.errorClassWithComma, ""));
+	    }else{
+		arg1.remove(ATTRIBUTE_CLASS);
+	    }
+	}
+    }
+}
---
+++ src/test/java/com/myamamoto/AppendErrorClassOnErrorBehaviorTest.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2012 m_yamamo0417
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.myamamoto;
+
+import org.apache.wicket.Component;
+import org.apache.wicket.markup.ComponentTag;
+import org.apache.wicket.markup.html.WebComponent;
+import org.apache.wicket.markup.parser.XmlTag;
+import org.apache.wicket.util.tester.WicketTester;
+import org.junit.Before;
+import org.junit.Test;
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+public class AppendErrorClassOnErrorBehaviorTest {
+
+    private WicketTester tester;
+    private AppendErrorClassOnErrorBehavior behavior;
+    private Component component;
+    private ComponentTag componentTag;
+
+    @Before
+    public void setUp() {
+	this.tester = new WicketTester();
+	this.behavior = new AppendErrorClassOnErrorBehavior();
+	this.component = new WebComponent("component");
+	this.componentTag = new ComponentTag(new XmlTag());
+    }
+
+    @Test
+    public void testOnError() {
+	this.component.error("error");
+	this.behavior.onComponentTag(component, componentTag);
+	assertThat(componentTag.getAttribute("class"), is("error"));
+    }
+
+    @Test
+    public void testOnValid() {
+	this.behavior.onComponentTag(component, componentTag);
+	assertThat(componentTag.getAttribute("class"), is(nullValue()));
+    }
+
+    @Test
+    public void testOnErrorWithDefault() {
+	this.componentTag.put("class", "cls");
+	this.component.error("error");
+	this.behavior.onComponentTag(component, componentTag);
+	assertThat(componentTag.getAttribute("class"), is("cls, error"));
+    }
+
+    @Test
+    public void testOnValidWithDefault() {
+	this.componentTag.put("class", "cls, error");
+	this.behavior.onComponentTag(component, componentTag);
+	assertThat(componentTag.getAttribute("class"), is("cls"));
+    }
+    
+}
